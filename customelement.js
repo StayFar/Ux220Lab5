8311bbfc9adfbc9bb6cd77c200b4bb14844a85d5
StayFar <farizahnaeem07@gmail.com>
2023-11-08 20:51:57 -0500

    add custom elements

--- customelement.js
+++ customelement.js
@@ -5,3 +5,20 @@ class FarElement extends HTMLElement{
 }
 
 customElements.define("x-far",FarElement);
+
+class CenterElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>
+        `; 
+    }
+}
+
+customElements.define("x-center",CenterElement);
+
+class YearElement extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = new Date().getFullYear();   
+    }
+}
+
+customElements.define("x-fullyear",YearElement);
